da41cf179f745f3935efeb9d3375abe9a564d004
watcharaphat <watcharaphat.3166@gmail.com>
2018-06-06 11:37:49 +0700

    expose file server port

--- docker/build-all-docker.sh
+++ docker/build-all-docker.sh
@@ -2,5 +2,5 @@
 
 docker build -f Dockerfile-abci -t watcharaphat/abci ..
 docker build -f Dockerfile-tendermint -t watcharaphat/tendermint .
-docker build -f Dockerfile-api -t watcharaphat/api ..
+docker build -f Dockerfile-api -t watcharaphat/tm-benchmark-api ..
 
